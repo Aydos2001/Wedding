915145e7ba39dbe99917640d54fdabb57049350a
Aydos2001 <nawrizbaevaydos15@gmail.com>
2023-11-15 12:25:46 +0500

    Udate about page

--- src/components/about-right.jsx
+++ src/components/about-right.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import sliderImg1 from "../images/galery-6.jpg"
+import { Carousel, IconButton } from "@material-tailwind/react";
 import { motion } from 'framer-motion'
 
 
@@ -10,49 +11,78 @@ const AboutRight = () => {
             whileInView={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: 20 }}
             transition={{ duration: 1, delay: 1 }}
-            id="gallery" className="relative w-full sm:py-[7%] h-full" data-carousel="slide">
-
-            <div className="relative h-[300px] md:h-full overflow-hidden rounded-[5px]">
-
-                <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                    <img src={"https://singaporebrides.com/articles/wp-content/uploads/2017/07/Raevian-and-Evans-Breathtakingly-Exotic-Pre-Wedding-Shoot-in-Xinjiang-29.jpg"} className="absolute block max-w-full min-w-full min-h-full object-cover -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="" />
-                </div>
-
-                <div className="hidden duration-700 ease-in-out" data-carousel-item="active">
-                    <img src="https://www.lovemydress.net/wp-content/uploads/2019/11/Edinburgh-wedding-photographer-videographer-1024x683.jpg" className="absolute block max-w-full min-w-full min-h-full object-cover -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="" />
-                </div>
-
-                <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                    <img src="https://st4.depositphotos.com/13194036/29979/i/450/depositphotos_299799490-stock-photo-handsome-smiling-bridegroom-putting-wedding.jpg" className="absolute block max-w-full min-w-full min-h-full object-cover -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="" />
-                </div>
-
-                <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                    <img src="https://media.istockphoto.com/id/1190043570/photo/happy-wedding-photography-of-bride-and-groom-at-wedding-ceremony-wedding-tradition-sprinkled.jpg?b=1&s=612x612&w=0&k=20&c=U5QcOqf91hMuvNLs-nO8VCxt0ThlaWw5sdzjSIBofls=" className="absolute block max-w-full min-w-full min-h-full object-cover -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="" />
-                </div>
-
-                <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                    <img src="https://images.squarespace-cdn.com/content/v1/5ba2a38bb27e396d3d619fee/1537550440546-X5GRAXL3W2L1W4FSH0QT/Montreal-Wedding-Photographer-Randy-Smith+%281+of+1%29-2.jpg" className="absolute block max-w-full min-w-full min-h-full object-cover -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="" />
-                </div>
-            </div>
-
-            <button type="button" className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-                <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                    <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4" />
-                    </svg>
-                    <span className="sr-only">Previous</span>
-                </span>
-            </button>
-            <button type="button" className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                    <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" />
-                    </svg>
-                    <span className="sr-only">Next</span>
-                </span>
-            </button>
+            className='sm:h-[90%] sm:my-[5%]'>
+            <Carousel
+                loop={true}
+                autoplay={true}
+                autoplayDelay={4000}
+                prevArrow={({ handlePrev }) => (
+                    <IconButton
+                        variant="text"
+                        color="white"
+                        size="lg"
+                        onClick={handlePrev}
+                        className="!absolute top-2/4 left-4 -translate-y-2/4"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={2}
+                            stroke="currentColor"
+                            className="h-6 w-6"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+                            />
+                        </svg>
+                    </IconButton>
+                )}
+                nextArrow={({ handleNext }) => (
+                    <IconButton
+                        variant="text"
+                        color="white"
+                        size="lg"
+                        onClick={handleNext}
+                        className="!absolute top-2/4 !right-4 -translate-y-2/4"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={2}
+                            stroke="currentColor"
+                            className="h-6 w-6"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                            />
+                        </svg>
+                    </IconButton>
+                )}
+                transition={{ duration: 1 }}
+                className="rounded-md">
+                <img
+                    src={"https://media.istockphoto.com/id/1334542509/photo/elopement-wedding.jpg?s=612x612&w=0&k=20&c=NMhdtBEyfTXtRH_XxYAh5kphkeewjl2rVDGQuLIfwTs="}
+                    alt="image 1"
+                    className="h-full w-full object-cover"
+                />
+                <img
+                    src={"https://img.freepik.com/free-photo/groom-putting-ring-bride-s-finger_1157-338.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699833600&semt=ais"}
+                    alt="image 2"
+                    className="h-full w-full object-cover"
+                />
+                <img
+                    src={"https://st3.depositphotos.com/1063633/37673/i/450/depositphotos_376734878-stock-photo-groom-leads-bride-hand-sunset.jpg"}
+                    alt="image 3"
+                    className="h-full w-full object-cover"
+                />
+            </Carousel>
         </motion.div>
-
 
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
-  ],
+  "./index.html", 
+  "./src/**/*.{vue,js,ts,jsx,tsx}",
+  "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+  "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+],
   theme: {
-    extend: {
-    },
+    extend: {},
     fontFamily : {
       mont : "Montserrat, sans-serif",
       lobster : "Lobster, sans-serif",
@@ -21,8 +22,6 @@ export default {
       xl : "1400px"
     }
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-}
+  plugins: [],
+});
 
