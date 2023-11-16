dca4e4c383fe1d71499797d47b8448f99679d248
Aydos2001 <nawrizbaevaydos15@gmail.com>
2023-11-16 21:20:07 +0500

    update anketa

--- src/Layout/LayoutPages/Questionnaire.jsx
+++ src/Layout/LayoutPages/Questionnaire.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Container from '../../components/container'
-import { Button, Input, Radio } from '@material-tailwind/react'
+import { Button, Radio } from '@material-tailwind/react'
 
 const Questionnaire = () => {
     return (
